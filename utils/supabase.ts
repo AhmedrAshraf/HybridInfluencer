9e6725306421b7dc3a31d8d34a8dde0a92c00526
AhmedrAshraf <ahmedr.0331@gmail.com>
2025-03-25 21:27:36 +0500

    minor change

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,5 +1,14 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const supabaseUrl = 'https://vwkjcwzfnpqskjtwwuxr.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3a2pjd3pmbnBxc2tqdHd3dXhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0MzQzODQsImV4cCI6MjA1NzAxMDM4NH0.zs5LSuk-ZAI3zxQocyNYe9_DH3qSOFxe4PRqL5isBXg'
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = 'https://vwkjcwzfnpqskjtwwuxr.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3a2pjd3pmbnBxc2tqdHd3dXhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0MzQzODQsImV4cCI6MjA1NzAxMDM4NH0.zs5LSuk-ZAI3zxQocyNYe9_DH3qSOFxe4PRqL5isBXg';
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false, // disable this in mobile apps
+  },
+});
